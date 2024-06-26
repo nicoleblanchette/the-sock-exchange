7db2e53184469b6474362be1a61273c8fbee9f01
Nicole Blanchette <nicoleblanchette05@gmail.com>
2024-06-26 16:50:05 -0400

    sock listings

---
+++ react/src/components/Promo.jsx
@@ -0,0 +1,14 @@
+export const Promo = ({ data }) => {
+  return (
+    <>
+      <div className="card">
+        <div className="card bg-light">
+          <div className="card-text">{data.feature}</div>
+          <div className="card-text">
+            <a href="#">Click to buy!</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
