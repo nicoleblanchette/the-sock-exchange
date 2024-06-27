9f258fad520644affc81d347bf75d375433b4297
Nicole Blanchette <nicoleblanchette05@gmail.com>
2024-06-27 11:31:09 -0400

    search and delete work

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -4,8 +4,44 @@ import promo_data from "./assets/promo.json";
 import { Footer } from "./components/Footer";
 import { Search } from "./components/Search";
 import { Promo } from "./components/Promo";
+import React, {useState, useEffect} from "react";
 
 function App() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await fetch(import.meta.env.VITE_SOCKS_API_URL);
+            if (!response.ok) {
+                throw new Error('Data could not be fetched!');
+            }
+            const json_response = await response.json();
+            setData(json_response); // assign JSON response to the data variable.
+        } catch (error) {
+            console.error('Error fetching socks:', error);
+        }
+    };
+
+    fetchData();
+}, []);
+
+const handleDelete = async (sockId) => {
+  try {
+      // Make an API request to delete the sock with the given sockId
+      const response = await fetch(`${import.meta.env.VITE_SOCKS_API_URL}/${sockId}`, {
+      method: 'DELETE',
+      });
+      if (!response.ok) {
+          throw new Error('Sock could not be deleted!');
+      }
+      // Update the state or fetch the updated data from the server
+      const updatedData = data.filter(sock => sock._id !== sockId); // Remove the deleted sock from the data array
+      setData(updatedData); // Update the state with the updated data
+  } catch (error) {
+      console.error('Error deleting sock:', error);
+  }
+};
+
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -73,7 +109,7 @@ function App() {
                 </a>
               </li>
             </ul>
-            <Search />
+            <Search setData={setData} />
           </div>
         </div>
       </nav>
@@ -87,16 +123,16 @@ function App() {
               style={{ gap: "20px", padding: "20px" }}
             >
               {/* Be an explorer and map */}
-              {promo_data.map((promo) => (
-                <Promo key={promo.id} data={promo} />
+              {promo_data.map((promo, idx) => (
+                <Promo key={idx} data={promo} />
               ))}
             </div>
             <div
               className="card-container"
               style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}
             >
-              {sock_data.map((sock) => (
-                <Sock key={sock.id} data={sock} />
+              {data.map((sock, idx) => (
+                <Sock key={idx} data={sock} handleDelete={handleDelete} />
               ))}
             </div>
             <Footer environment={"Development"} />
--- react/src/components/Footer.jsx
+++ react/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export const Footer = ({environment}) => {
   return (
-    <footer className="text-muted">
+    <footer className={`text-muted ${import.meta.env.VITE_ENVIRONMENT === 'development' ? 'bg-yellow' : 'bg-green'}`} style={{fontWeight: "bold"}}>
       <div>
         <strong>{environment}</strong>
       </div>
