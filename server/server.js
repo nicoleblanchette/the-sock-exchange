b5abb89d0e47ccfc23e387ba0080d564c2470a91
Nicole Blanchette <nicoleblanchette05@gmail.com>
2024-06-28 13:10:16 -0400

    mongodb added

--- server/server.js
+++ server/server.js
@@ -1,5 +1,13 @@
 import express from "express";
 import { promises as fs } from "fs";
+import { MongoClient, ObjectId } from 'mongodb';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+const url = process.env.MONGO_DB_URL;
+const dbName = process.env.MONGO_DB;
+const collectionName = process.env.MONGO_DB_COLLECTION;
 
 const app = express();
 const PORT = 3000;
